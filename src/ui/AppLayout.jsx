ceca0f085035b4d606dc65eef596cf08d6fa310a
Moussa-Kalam <m.amzat@alustudent.com>
2023-12-31 18:37:28 +0200

    style: home page components

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -10,14 +10,16 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className='layout'>
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {/* Show the loader if the router is loading */}
       {isLoading && <Loader />}
       <Header />
 
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-auto">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/users/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to='/'>Fast React Pizza Co.</Link>
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6 flex items-center justify-between">
+      <Link to="/" className="tracking-[5px]">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      Moussa
+      <Username />
     </header>
   );
 }
