94e42033640ad2b7183feeb40c092b5f35e069f0
Moussa-Kalam <m.amzat@alustudent.com>
2023-12-31 23:35:42 +0200

    style: improve the loading indicator

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monoscape",
+    },
+
+    extend: {
+      height: {
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
